7a83300cba8c5163097282dd0d91a28302609b7e
PureDevPer <wooseok.kim1207@gmail.com>
2019-08-12 10:08:11 -0400

    Dynamic Components - Added ID

--- src/App.js
+++ src/App.js
@@ -15,16 +15,19 @@ const Food = ({ name, picture }) => {
 
 const foods = [
 	{
+		id: 1,
 		name: 'pizza',
 		image:
 			'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-keto-pizza-073-1544039876.jpg?crop=0.668xw:1.00xh;0.233xw,0.00255xh&resize=980:*'
 	},
 	{
+		id: 2,
 		name: 'chicken',
 		image:
 			'https://www.tasteofhome.com/wp-content/uploads/2018/01/Southern-Fried-Chicken-with-Gravy_exps33285_THRAA2874593C01_23_1b_RMS-696x696.jpg'
 	},
 	{
+		id: 3,
 		name: 'Spam',
 		image:
 			'https://images2.minutemediacdn.com/image/upload/c_crop,h_1576,w_2800,x_0,y_52/f_auto,q_auto,w_1100/v1554931909/shape/mentalfloss/20997-istock-471531747.jpg'
@@ -36,7 +39,7 @@ function App() {
 		<div>
 			<h1>Hello React!!</h1>
 			{foods.map(menu => (
-				<Food name={menu.name} picture={menu.image} />
+				<Food key={menu.id} name={menu.name} picture={menu.image} />
 			))}
 
 			<Phone fav="galaxy" />
