fbca2059592cae0ed600566f1d27725179326420
PureDevPer <wooseok.kim1207@gmail.com>
2019-08-12 10:34:39 -0400

    State

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 - Component: is the function which returns HTML
 - Props: Property, Most components can be customized when they are created, with different parameters. These creation parameters are called `props`
   - PropTypes: https://reactjs.org/docs/typechecking-with-proptypes.html
+- State: An Object that determines how the component renders and behaves. In other words, `state` is what allows you to create components that are dynamic and interactive.
--- src/App.js
+++ src/App.js
@@ -1,67 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Phone = props => {
-	return <h2>I like {props.fav}</h2>;
-};
+class App extends React.Component {
+	state = {
+		count: 0
+	};
+	add = () => {
+		this.setState(val => ({ count: ++val.count }));
+	};
 
-const Food = ({ name, picture, rating }) => {
-	return (
-		<div>
-			<h3>This is {name}</h3>
-			<h4>{rating} / 5</h4>
-			<img src={picture} alt={name} />
-		</div>
-	);
-};
+	minus = () => {
+		this.setState({ count: --this.state.count });
+	};
 
-Food.propTypes = {
-	name: PropTypes.string.isRequired,
-	picture: PropTypes.string.isRequired,
-	rating: PropTypes.number.isRequired
-};
-
-const foods = [
-	{
-		id: 1,
-		name: 'pizza',
-		image:
-			'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-keto-pizza-073-1544039876.jpg?crop=0.668xw:1.00xh;0.233xw,0.00255xh&resize=980:*',
-		rating: 5
-	},
-	{
-		id: 2,
-		name: 'chicken',
-		image:
-			'https://www.tasteofhome.com/wp-content/uploads/2018/01/Southern-Fried-Chicken-with-Gravy_exps33285_THRAA2874593C01_23_1b_RMS-696x696.jpg',
-		rating: 4
-	},
-	{
-		id: 3,
-		name: 'Spam',
-		image:
-			'https://images2.minutemediacdn.com/image/upload/c_crop,h_1576,w_2800,x_0,y_52/f_auto,q_auto,w_1100/v1554931909/shape/mentalfloss/20997-istock-471531747.jpg',
-		rating: 3
+	render() {
+		return (
+			<div>
+				<h1>The number is: {this.state.count}</h1>
+				<button onClick={this.add}>Add</button>
+				<button onClick={this.minus}>Minus</button>
+			</div>
+		);
 	}
-];
-
-function App() {
-	return (
-		<div>
-			<h1>Hello React!!</h1>
-			{foods.map(menu => (
-				<Food
-					key={menu.id}
-					name={menu.name}
-					picture={menu.image}
-					rating={menu.rating}
-				/>
-			))}
-
-			<Phone fav="galaxy" />
-			<Phone fav="iPhone" />
-		</div>
-	);
 }
 
 export default App;
