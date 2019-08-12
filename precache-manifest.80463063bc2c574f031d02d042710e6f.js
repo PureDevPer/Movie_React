969f1e379abd9cb916b00a21979c61606efeb5c0
PureDevPer <wooseok.kim1207@gmail.com>
2019-08-12 12:05:35 -0400

    Updates

---
+++ precache-manifest.80463063bc2c574f031d02d042710e6f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c3fe5a9c4ae479fd32e8b27c347d2b9",
+    "url": "/Movie_React/index.html"
+  },
+  {
+    "revision": "c0b8e663157de6ee76f0",
+    "url": "/Movie_React/static/css/main.012408ea.chunk.css"
+  },
+  {
+    "revision": "a22986ed881a5224a136",
+    "url": "/Movie_React/static/js/2.c870cff4.chunk.js"
+  },
+  {
+    "revision": "c0b8e663157de6ee76f0",
+    "url": "/Movie_React/static/js/main.3184e716.chunk.js"
+  },
+  {
+    "revision": "ba290c80110162bd7454",
+    "url": "/Movie_React/static/js/runtime~main.960cbd4c.js"
+  }
+]);
